6f1bc9f5a343696f650ae533fd37829eabbef9d8
Tyler Mercer <acct@tylermercer.net>
2026-09-23 18:31:58 -0600

    Project setup via init.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import rawFonts from './lib/plugins/vite/rawFonts.js';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astroflare.tmercer.workers.dev',
+  site: 'https://mercer-fam-recipes.tmercer.workers.dev',
   adapter: cloudflare(),
   markdown: {
     processor: unified(),
